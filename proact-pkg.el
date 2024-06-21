4d5ba444d077fcf96bc26666699c2505405c90ac
Rafael Campos Nunes <rcamposnunes@outlook.com>
2024-06-20 23:44:02 -0300

    Change the used commit to point to tag 1.0
    Signed-off-by: Rafael Campos Nunes <rcamposnunes@outlook.com>

--- proact-pkg.el
+++ proact-pkg.el
@@ -1,6 +1,6 @@
 (define-package "proact-mode" "20240620.00" "Major mode for the act programming language"
   '((emacs "26.1"))
-  :commit "eb081c3bfc0b688390a3918cf13af99c8d2853cd"
+  :commit "fc7e1ad8ea1cf71f6abce03462358d5b3e0378af"
   :authors '(("The proact-mode Authors"))
   :maintainer '("The proact-mode Authors")
   :maintainers '(("The proact-mode Authors"))y
