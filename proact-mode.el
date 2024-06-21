fc7e1ad8ea1cf71f6abce03462358d5b3e0378af
Rafael Campos Nunes <rcamposnunes@outlook.com>
2024-06-20 23:41:02 -0300

    Fix linting problems
    Signed-off-by: Rafael Campos Nunes <rcamposnunes@outlook.com>

--- proact-mode.el
+++ proact-mode.el
@@ -1,4 +1,4 @@
-;;; proact-mode.el --- major mode for editing ACT files -*- coding: utf-8; lexical-binding: t; -*-
+;;; proact-mode.el --- Major mode for the ACT programming language -*- coding: utf-8; lexical-binding: t; -*-
 
 ;;; Commentary:
 
@@ -7,39 +7,46 @@
 ;; can be found in the LICENSE file.
 
 ;; Authors: Rafael Campos Nunes <rcamposnunes@outlook.com>
+;; Package-Version: 1.0
+;; Package-Requires: ((emacs "26.1"))
+;; URL: https://github.com/rafaelcn/proact
+
+;;
+;; This file is not part of GNU Emacs.
+;;
 
 ;;; Code:
 
-(defvar act-keywords '("export" "import") "List of keywords in act.")
-(defvar act-types '("bool" "e1of" "e2of" "e3of" "globals") "List of types in act.")
-(defvar act-functions '("defproc" "prs") "List of functions in act.")
+(defvar proact-keywords '("export" "import") "List of keywords in act.")
+(defvar proact-types '("bool" "e1of" "e2of" "e3of" "globals") "List of types in act.")
+(defvar proact-functions '("defproc" "prs") "List of functions in act.")
 
-(defvar act-fontlock nil "List for font-lock defaults.")
+(defvar proact-fontlock nil "List for font-lock defaults.")
 
-(setq act-fontlock
-	  (let ((act-comments-regex "//.*")
-			(act-highlights "<[[:digit:]]+>")
-			(act-keywords-regex (regexp-opt act-keywords 'words))
-			(act-types-regex (regexp-opt act-types 'words))
-			(act-functions-regex (regexp-opt act-functions 'words)))
+(setq proact-fontlock
+	  (let ((proact-comments-regex "//.*")
+			(proact-highlights "<[[:digit:]]+>")
+			(proact-keywords-regex (regexp-opt proact-keywords 'words))
+			(proact-types-regex (regexp-opt proact-types 'words))
+			(proact-functions-regex (regexp-opt proact-functions 'words)))
 
 		;; a regex for each category of word within the language
 
-		(list (cons act-keywords-regex 'font-lock-keyword-face)
-			  (cons act-functions-regex 'font-lock-function-name-face)
-			  (cons act-types-regex 'font-lock-type-face)
-			  (cons act-comments-regex 'font-lock-comment-face)
-			  (cons act-highlights 'font-lock-constant-face))))
+		(list (cons proact-keywords-regex 'font-lock-keyword-face)
+			  (cons proact-functions-regex 'font-lock-function-name-face)
+			  (cons proact-types-regex 'font-lock-type-face)
+			  (cons proact-comments-regex 'font-lock-comment-face)
+			  (cons proact-highlights 'font-lock-constant-face))))
 
 
 ;;;###autoload
-(define-derived-mode act-mode prog-mode "proact"
+(define-derived-mode proact-mode prog-mode "proact"
   "Major mode for the act programming language."
 
   (setq-local font-lock-defaults '((act-fontlock))))
 
 (add-to-list 'auto-mode-alist '("\\.act\\'" . act-mode))
 
-(provide 'act-mode)
+(provide 'proact-mode)
 
 ;;; proact-mode.el ends here
