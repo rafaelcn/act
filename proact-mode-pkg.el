35e0338029ba31f42dc3026b418edd24afb2a803
Rafael Campos Nunes <rcamposnunes@outlook.com>
2024-06-20 23:25:15 -0300

    Add a -pkg file
    Signed-off-by: Rafael Campos Nunes <rcamposnunes@outlook.com>

---
+++ proact-mode-pkg.el
@@ -0,0 +1,12 @@
+(define-package "proact-mode" "20240620.00" "Major mode for the act programming language"
+  '((emacs "26.1"))
+  :commit "eb081c3bfc0b688390a3918cf13af99c8d2853cd"
+  :authors '(("The proact-mode Authors"))
+  :maintainer '("The proact-mode Authors")
+  :maintainers '(("The proact-mode Authors"))y
+  :keywords '("languages" "act" "proact")
+  :url "https://github.com/rafaelcn/proact-mode.el")
+
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
