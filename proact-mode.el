de334c4f31cd843f04546b2ce823406ad78afa3c
Rafael Campos Nunes <rcamposnunes@outlook.com>
2024-07-17 19:21:36 -0300

    Change the name of the mode from proact to act
    Signed-off-by: Rafael Campos Nunes <rcamposnunes@outlook.com>

--- proact-mode.el
+++ proact-mode.el
@@ -1,50 +1,51 @@
-;;; proact-mode.el --- Major mode for the ACT programming language -*- coding: utf-8; lexical-binding: t; -*-
-
-;;; Commentary:
-
-;; SPDX-License-Identifier: MIT
+;;; act-mode.el --- Major mode for the ACT programming language -*- coding: utf-8; lexical-binding: t; -*-
 
 ;; Authors: Rafael Campos Nunes <rcamposnunes@outlook.com>
 ;; Package-Version: 1.0
 ;; Package-Requires: ((emacs "26.1"))
-;; URL: https://github.com/rafaelcn/proact
+;; URL: https://github.com/rafaelcn/act
+;; SPDX-License-Identifier: MIT
 
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
 
+;;; Commentary:
+
+;; This mode currently only supports syntax highlighting.
+
 ;;; Code:
 
-(defvar proact-keywords
+(defvar act-keywords
   '("export" "import")
   "List of keywords in act.")
 
-(defvar proact-types
+(defvar act-types
   '("preal" "pint" "bool" "int" "e1of" "e2of" "e3of" "c1of" "globals" "globals_np")
   "List of types in act.")
 
-(defvar proact-functions
+(defvar act-functions
   '("defproc" "deftype" "defchan" "prs")
   "List of functions in act.")
 
-(defvar proact-fontlock
+(defvar act-fontlock
   ;; a regex for each category of word within the language
   (list
    (cons "//.*" 'font-lock-comment-face)                                       ;; comments regex
-   (cons (regexp-opt proact-keywords 'words) 'font-lock-keyword-face)          ;; keywords regex
-   (cons (regexp-opt proact-functions 'words) 'font-lock-function-name-face)   ;; functions regex
-   (cons (regexp-opt proact-types 'words) 'font-lock-type-face)                ;; types regex
+   (cons (regexp-opt act-keywords 'words) 'font-lock-keyword-face)          ;; keywords regex
+   (cons (regexp-opt act-functions 'words) 'font-lock-function-name-face)   ;; functions regex
+   (cons (regexp-opt act-types 'words) 'font-lock-type-face)                ;; types regex
    (cons "<[[:digit:]]+>" 'font-lock-constant-face))                           ;; highlights regex
   "List for font-lock defaults.")
 
 ;;;###autoload
-(define-derived-mode proact-mode prog-mode "proact"
+(define-derived-mode act-mode prog-mode "act"
   "Major mode for the act programming language."
   
-  (setq-local font-lock-defaults '((proact-fontlock))))
+  (setq-local font-lock-defaults '((act-fontlock))))
 
-(add-to-list 'auto-mode-alist '("\\.act\\'" . proact-mode))
+(add-to-list 'auto-mode-alist '("\\.act\\'" . act-mode))
 
-(provide 'proact-mode)
+(provide 'act-mode)
 
-;;; proact-mode.el ends here
+;;; act-mode.el ends here
